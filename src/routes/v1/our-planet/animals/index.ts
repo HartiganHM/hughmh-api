6e1834bdcda676322c3ea2093b9c551b4bac8d97
Hugh Hartigan <hartigan.hm@gmail.com>
2025-12-29 15:14:30 -0700

    Create Prisma singleton

--- src/routes/v1/our-planet/animals/index.ts
+++ src/routes/v1/our-planet/animals/index.ts
@@ -1,7 +1,5 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '../../../../lib/prisma';
 
 export default async function (server: FastifyInstance) {
   server.get(
